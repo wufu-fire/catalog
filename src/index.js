647370d52d02956d0c5028dd83eb51b80aa00794
wangxuan14 <wangxuan14@wangxuan14deMacBook-Pro.local>
2018-06-05 16:31:20 +0800

    feat: add files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import App from './page/index.vue'
+
+new Vue({
+	el: '#app',
+	render: h => h(App)
+})
---
+++ src/page/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="app">
+    fffff
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'app',
+    components: {
+        
+    },
+    mounted(){
+        alert(1)
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+</style>
