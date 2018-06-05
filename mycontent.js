5c2f64d2ded029c81f3b33b210ae986a8434ba6c
wangxuan14 <wangxuan14@wangxuan14deMacBook-Pro.local>
2018-06-05 16:14:40 +0800

    feat: delete all

--- mycontent.js
+++
@@ -1,7 +0,0 @@
-var $body = document.querySelector("body"),
-    $catalog = document.createElement("div"),
-    text = "sdjfasdfjasfjasdfjasdf";
-
-$catalog.innerHTML = text
-$catalog.style.color = "red"
-$body.appendChild($catalog)   
